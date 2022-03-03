b0924a91f43262f879ce34ee961bddab1f057212
NaHyeon <nahyeonee99@gmail.com>
2022-03-03 16:54:05 +0900

    입력받은 월의 최대 일수 출력하기

--- src/playground/Calendar.java
+++ src/playground/Calendar.java
@@ -4,12 +4,20 @@ import java.util.Scanner;
 
 public class Calendar {
     public static void main(String[] args) {
-        System.out.println("두 수를 입력하세요.");
+        // 31일 - 1,3,5,7,8,10,12월
+        // 30일 - 4,6,9,11월
+        // 28,29일 - 2월
+
         Scanner scanner = new Scanner(System.in);
-        int num1 = scanner.nextInt();
-        String num2 = scanner.next();
-        int num2_int = Integer.parseInt(num2);
-        System.out.println("두 수의 합은 " + (num1+num2_int) + " 입니다.");
-        System.out.println("두 수의 합은 " + (num1+num2) + " 입니다.");
+        System.out.println("달을 입력하세요.");
+        int input = scanner.nextInt();
+
+        int[] array = {31,30,28,30,31,30,31,31,30,31,30,31};
+
+        for(int i = 0; i<12; i++) {
+            if(input-1 == i) {
+                System.out.println(i+1+ "월은 " + array[i]+"일까지 있습니다.");
+            }
+        }
     }
 }
