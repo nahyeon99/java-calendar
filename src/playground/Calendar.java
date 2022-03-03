52207a52511946837ee796f239d160340b62a6bb
NaHyeon <nahyeonee99@gmail.com>
2022-03-03 14:29:01 +0900

    add <두 수의 합을 구하는 프로그램 작성하기

--- src/playground/Calendar.java
+++ src/playground/Calendar.java
@@ -1,7 +1,15 @@
 package playground;
 
+import java.util.Scanner;
+
 public class Calendar {
     public static void main(String[] args) {
-        System.out.println("Hello, Calendar");
+        System.out.println("두 수를 입력하세요.");
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        String num2 = scanner.next();
+        int num2_int = Integer.parseInt(num2);
+        System.out.println("두 수의 합은 " + (num1+num2_int) + " 입니다.");
+        System.out.println("두 수의 합은 " + (num1+num2) + " 입니다.");
     }
 }
