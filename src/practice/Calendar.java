938f3eedc2baa8f2a9188baa2e90ee61072c3386
NaHyeon <nahyeonee99@gmail.com>
2022-03-05 13:36:16 +0900

    진짜 달력 자동으로 찍기 (2)

--- src/practice/Calendar.java
+++ src/practice/Calendar.java
@@ -1,10 +1,10 @@
 package practice;
 
 public class Calendar {
-    int[] MAX_DAYS = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-    int[] LEAP_MAX_DAYS = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+    private static int[] MAX_DAY = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static int[] LEAP_MAX_DAY = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    // 해당 년도가 윤년인지 평년인지 확인하는 메소드
+    // 윤년,평년 체크하는 메소드
     private boolean isLeapYear(int year) {
         if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
             return true;
@@ -12,60 +12,55 @@ public class Calendar {
             return false;
         }
     }
-
-    // 해당 월의 maxDay 리턴해주는 메소드
-    private int getMaxDay(int year, int month) {
-        if(isLeapYear(year)) {
-            return LEAP_MAX_DAYS[month];
-        } else {
-            return MAX_DAYS[month];
-        }
+    // 달의 maxDay 리턴해주는 메소드
+    private int getMaxDayOfMonth(int year, int month) {
+        int maxDay = isLeapYear(year) ? LEAP_MAX_DAY[month] : MAX_DAY[month];
+        return maxDay;
     }
 
+
     public void printCalendar(int year, int month) {
-        System.out.printf(" << %4d년 %3d월 >> \n", year, month);
+        System.out.printf(" << %4d월 %3d일 >>\n", year, month);
         System.out.println(" SU MO TU WE TH FR SA");
         System.out.println("---------------------");
 
-        // 요일 check
-        // 일:0, 월:1, 화:2, 수:3, 목:4, 금:5, 토:6
-        int weekday = getWeekday(year, month, 1);
+        // 1일의 요일 알아내기
+        int weekday = getWeekday(year, month);
 
-        int cnt = 0;
+        // 첫 주, 해당 요일만큼 공간 띄우기
         for(int i = 0; i<weekday; i++) {
             System.out.print("   ");
-            cnt += 1;
         }
-        int maxDay = getMaxDay(year, month);
 
-        for(int i = 1; i<=maxDay; i++) {
+        for(int i = 1; i<=getMaxDayOfMonth(year, month); i++) {
             System.out.printf("%3d", i);
-            if((i+cnt) % 7 == 0) System.out.println();
+            if((i+weekday) % 7 == 0) System.out.println();
         }
         System.out.println();
-
     }
-    private int getWeekday(int year, int month, int day) {
-        int sYear = 1970;
-        final int STANDARD_WEEKDAY = 4; // 1970/1/1 목요일:4 기준
 
+    private int getWeekday(int year, int month) {
+        int STANDARD_WEEKDAY = 4; // 1970/1/1 목 기준
+
+        int sYear = 1970;
         int cnt = 0;
 
-        // year 차이만큼 더하기
-        for(int i = sYear; i<year; i++) {
-            // 윤년이면 cnt += 366, 평년이면 cnt += 365
+        // 기준 날짜로부터 년도 차이만큼 day 더하기
+        // 고려해야할 점: 윤년인가 평년인가
+        for(int i = sYear; i < year; i++) {
+            // 윤년인지 확인
             int delta = isLeapYear(i) ? 366 : 365;
             cnt += delta;
         }
 
-        // month 차이만큼 더하기
+        // 기준 월로부터 일자 차이만큼 day 더하기
         for(int i = 1; i<month; i++) {
-            cnt += getMaxDay(year, i);
+            int delta = getMaxDayOfMonth(year, i);
+            cnt += delta;
         }
-        cnt += day - 1;
 
-        // 요일 판단
-        int weekday = (cnt + STANDARD_WEEKDAY) % 7;
+        int weekday = (cnt+STANDARD_WEEKDAY) % 7;
+
         return weekday;
     }
 }
--- src/practice/Prompt.java
+++ src/practice/Prompt.java
@@ -3,29 +3,27 @@ package practice;
 import java.util.Scanner;
 
 public class Prompt {
-    public void runPrompt() {
+    private static void runPrompt() {
         Scanner scanner = new Scanner(System.in);
 
         int year = -1;
         int month = -1;
+
         while(true) {
             System.out.println("년도를 입력하세요. (exit: -1)");
-            System.out.print("YEAR> ");
             year = scanner.nextInt();
             if(year == -1) break;
-
             System.out.println("달을 입력하세요. (exit: -1)");
-            System.out.print("MONTH> ");
             month = scanner.nextInt();
             if(month == -1) break;
 
             Calendar cal = new Calendar();
             cal.printCalendar(year, month);
         }
+
         System.out.println("bye~");
         scanner.close();
     }
-
     public static void main(String[] args) {
         Prompt p = new Prompt();
         p.runPrompt();
