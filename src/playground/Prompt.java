eba7ddf0a23624e1898228b7e156ed19e7f0866c
NaHyeon <nahyeonee99@gmail.com>
2022-03-08 06:29:24 +0900

    Remove String if-else

--- src/playground/Prompt.java
+++ src/playground/Prompt.java
@@ -3,7 +3,7 @@ package playground;
 import java.text.ParseException;
 import java.util.Scanner;
 
-public class Prompt<T> {
+public class Prompt {
     public void printMenu() {
         System.out.println("+----------------------+");
         System.out.println("| 1. 일정 등록");
@@ -13,6 +13,58 @@ public class Prompt<T> {
         System.out.println("+----------------------+");
         System.out.println("명령 (1, 2, 3, h, q)");
         System.out.print("입력 > ");
+    }
+
+    public int parseDay(String week) {
+        switch(week) {
+            case "su":
+                return 0;
+            case "mo":
+                return 1;
+            case "tu":
+                return 2;
+            case "we":
+                return 3;
+            case "th":
+                return 4;
+            case "fr":
+                return 5;
+            case "sa":
+                return 6;
+            default:
+                return 0;
+        }
+    }
+
+    private void runPrompt() throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        Calendar cal = new Calendar();
+
+        boolean isLoop = true;
+        while(isLoop) {
+            printMenu();
+            String cmd = scanner.next();
+
+            switch(cmd) {
+                case "1":
+                    cmdRegister(scanner, cal);
+                    break;
+                case "2":
+                    cmdSearch(scanner, cal);
+                    break;
+                case "3":
+                    cmdCal(scanner, cal);
+                    break;
+                case "h":
+                    printMenu();
+                    break;
+                case "q":
+                    isLoop = false;
+                    break;
+            }
+        }
+        System.out.println("Bye");
+        scanner.close();
     }
 
     private void cmdSearch(Scanner s, Calendar c) {
@@ -55,23 +107,6 @@ public class Prompt<T> {
             return;
         }
         c.printCalendar(year, month);
-    }
-
-    private void runPrompt() throws ParseException {
-        Scanner scanner = new Scanner(System.in);
-        Calendar cal = new Calendar();
-
-        while(true) {
-            printMenu();
-            String cmd = scanner.next();
-            if(cmd.equals("1")) cmdRegister(scanner, cal);
-            else if(cmd.equals("2")) cmdSearch(scanner, cal);
-            else if(cmd.equals("3")) cmdCal(scanner, cal);
-            else if(cmd.equals("4")) printMenu();
-            else if(cmd.equals("q")) break;
-        }
-        System.out.println("Bye");
-        scanner.close();
     }
 
     public static void main(String[] args) throws ParseException {
